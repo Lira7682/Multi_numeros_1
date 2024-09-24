1b9df85db8f5d11b05e5ea9e70bc9b5e9552a59e
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-23 20:35:29 -0600

    Add files via upload

---
+++ Multi_numeros_1/multi_numeros (1).py
@@ -0,0 +1,12 @@
+print (" ") #Espacio a agregar 
+print ("Lira Hernandez Dayana Yamileth: Practica que realiza multiplicacion de tres numeros")
+print (" ")
+
+a = int(input('Ingresa 1er numero:'))  #intruccion que te pide ingresar un numero 
+b = int(input('Ingresa 2do numero:'))  #intruccion que te pide ingresar un numero 
+c = int(input('Ingresa 3er numero:'))  #intruccion que te pide ingresar un numero 
+
+multiplicacion = a * b* c  #Operacion a realizar
+print ("El resultado de la multiplicacion es:", multiplicacion)  #Resultado de la operacion 
+
+print (" ")
